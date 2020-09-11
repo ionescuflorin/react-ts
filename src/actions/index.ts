f89e21091b89aca313d849107f328da4348596f8
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-09-11 17:17:28 +0300

    Adding delete base functionality

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
-import { ActionTypes } from './types'
+import { ActionTypes } from './types';
 
 export interface Todo {
   id: number;
@@ -13,6 +13,12 @@ export interface FetchTodosAction {
   payload: Todo[];
 }
 
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  // because we provide only the id we want to delete
+  payload: number;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
@@ -21,8 +27,15 @@ export const fetchTodos = () => {
     const response = await axios.get<Todo[]>(url);
 
     dispatch<FetchTodosAction>({
-        type: ActionTypes.fetchTodos,
-        payload: response.data
-    })
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    });
   };
 };
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id
+  }
+}

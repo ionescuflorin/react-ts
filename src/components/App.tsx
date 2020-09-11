6a0b714a794713b39f4f3ed12b3984f3d93891ea
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-09-11 17:12:38 +0300

    Render a list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,8 +7,23 @@ interface AppProps {
   fetchTodos(): any;
 }
 class _App extends Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
   render() {
-    return <div>Hi</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch todos</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 

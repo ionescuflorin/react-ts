22c08ef6609308ca38f6dcefe2c09f5d35218d7f
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-09-11 17:43:46 +0300

    Add delete functionality and loading functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions/index';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
+import { isNullOrUndefined } from 'util';
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
-class _App extends Component<AppProps> {
+
+interface AppState {
+  fetching: boolean;
+}
+
+class _App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = {
+      fetching: false,
+    };
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ fetching: true });
   };
 
   renderList(): JSX.Element[] {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id}>{todo.title}</div>;
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
     });
   }
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
 
   render() {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch todos</button>
+        {this.state.fetching ? 'LOADING' : null}
         {this.renderList()}
       </div>
     );
@@ -39,4 +70,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
 };
 
 // dont use default in typescript
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,9 +1,13 @@
 import { Todo, Action, ActionTypes } from '../actions';
 
 export const todosReducer = (state: Todo[] = [], action: Action) => {
+// action - here it wil be one of the 2 types
+// here switch statement it will work like a type guard and it will be only one value
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return  state.filter((todo: Todo) => todo.id !== action.payload)
     default:
       return state;
   }
